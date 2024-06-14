af247b76054b3f2bf3082002424f4fa7631728d8
Jacob Tarun <jacobtarun782@outlook.com>
2024-06-14 10:17:20 +0800

    fix borgar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggle_nav() {
     var navbar = document.getElementById("navbar");
     navbar.classList.toggle("nav_shown");
-    var main = document.getElementsByTagName("main")[0];
-    main.classList.toggle("nav_shown");
+    // var main = document.getElementsByTagName("main")[0];
+    // main.classList.toggle("nav_shown");
 }
